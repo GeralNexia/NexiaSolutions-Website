7f275f24836f7ef0eecac0997dd44ebc0681aee2
GeralNexia <geralnexia@gmail.com>
2026-09-21 18:55:14 +0100

    Usar hash history e React Client Root no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { createRouter } from './router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 import './styles.css'
 
-const router = createRouter()
+// Criação do router com HashHistory para evitar problemas de rotas e subpastas no GitHub Pages
+const hashHistory = createHashHistory()
+
+const router = createRouter({
+  routeTree,
+  history: hashHistory,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const rootElement = document.getElementById('root')!
 
