c83d320febafe44caf00e2e8ebc377bf276e6d69
GeralNexia <geralnexia@gmail.com>
2026-09-21 18:35:56 +0100

    Corrigir imports do Tailwind no styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
+import './index.css' // <-- Adiciona esta linha
 
 const router = createRouter()
 
