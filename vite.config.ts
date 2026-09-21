7f83a96bddd2346597b944cd58c0e00c6f0ceb39
GeralNexia <geralnexia@gmail.com>
2026-09-21 19:11:54 +0100

    Corrigir o caminho base para o GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  base: '/NexiaSolutions-Website/', // <--- Adiciona esta linha
   plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
   css: {
     transformer: 'postcss',
